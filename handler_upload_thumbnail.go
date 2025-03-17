8b96f688c98eea701c0eb1633590e1b7fdaedf48
stuartw <stuwilson@gmail.com>
2025-03-17 21:00:25 +0000

    thumbnail files now stores in /assets/ and filetype is checked

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
-	"encoding/base64"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,7 +35,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
 	// Set maxMemory to 10MB (10 bitshifted 20 times to left)
 	const maxMemory = 10 << 20
 	r.ParseMultipartForm(maxMemory)
@@ -40,39 +42,64 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fileData, fileHeader, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "could not get file data/header", err)
+		return
 	}
 	mediaType := fileHeader.Header.Get("Content-type")
-	imageData, err := io.ReadAll(fileData)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "could not read file data", err)
+	mimeType, _, err := mime.ParseMediaType(mediaType)
+
+	if mimeType != "image/jpeg" || mimeType != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "thumbnail files must be jpeg/png", err)
+		return
 	}
 
 	videoData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "could not get video data from db", err)
+		return
 	}
 
 	if videoData.UserID != userID {
 		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
-	// newThumbnail := thumbnail{
-	// 	imageData,
-	// 	mediaType,
-	// }
+	imageExt, err := getImageFileType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "image filetype not found", err)
+		return
+	}
+	thumbnailFilepath := filepath.Join(cfg.assetsRoot, videoIDString+imageExt)
+	newFile, err := os.Create(thumbnailFilepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create save image file", err)
+		return
+	}
+	defer newFile.Close()
 
-	//videoThumbnails[videoData.ID] = newThumbnail
+	_, err = io.Copy(newFile, fileData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to save image file", err)
+		return
+	}
+	thumbnailURL := fmt.Sprintf("http://localhost:%v/assets/%v%v", cfg.port, videoIDString, imageExt)
 
-	encodedImage := base64.StdEncoding.EncodeToString(imageData)
-	dataURL := "data:"+mediaType+";base64,"+encodedImage
+	videoData.ThumbnailURL = &thumbnailURL
 
-	//thumbnailURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, videoIDString)
-	
-	videoData.ThumbnailURL = &dataURL
 	err = cfg.db.UpdateVideo(videoData)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "could not update video db", err)
+		return
 	}
 
 	respondWithJSON(w, http.StatusOK, videoData)
+}
+
+func getImageFileType(s string) (string, error) {
+	if strings.HasPrefix(s, "image/") {
+		parts := strings.Split(s, "/")
+		if len(parts) == 2 {
+			return "." + parts[1], nil
+		}
+	}
+	return "", fmt.Errorf("no image filetype found")
 }
